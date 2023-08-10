f01f04219854abf7985d3866869de9f8f93bad99
mohammedjaber <jaber@>
2023-08-10 13:28:24 +0300

    jaber APi Work

---
+++ app/src/main/java/com/blueray/alqudra/api/inProgressRides/DeliveryBranchLatLong.kt
@@ -0,0 +1,6 @@
+package com.blueray.alqudra.api.inProgressRides
+
+data class DeliveryBranchLatLong(
+    val lat: Double,
+    val lon: Double
+)
---
+++ app/src/main/java/com/blueray/alqudra/api/inProgressRides/ReceivingBranchLatLong.kt
@@ -0,0 +1,6 @@
+package com.blueray.alqudra.api.inProgressRides
+
+data class ReceivingBranchLatLong(
+    val lat: Double,
+    val lon: Double
+)
